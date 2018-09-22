3798d392b6ed152adb67a74364cb23f501477756
nicejade <jeffygisgreat@gmail.com>
2018-09-22 23:57:37 +0800

    ✨ update service-worker

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,10 @@ var ignoreCache = [
   /https?:\/\/cdn.bootcss.com\//,
   /https?:\/\/api.github.com\//,
   /https?:\/\/www.google-analytics.com\//,
+  /https?:\/\/js.t.sinajs.cn\//,
+  /https?:\/\/tjs.sjs.sinajs.cn\//,
+  /https?:\/\/image.nicelinks.site\//,
+  /webpack:\//,
   /chrome-extension:\//
 ];
 var port;
